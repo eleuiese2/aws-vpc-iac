4506b314018811a7c4d067d40c2cf8612e6e4aac
eleuiese2 <luisclape@gmail.com>
2025-04-06 23:17:35 -0500

    Create module

--- variables.tf
+++ variables.tf
@@ -7,12 +7,11 @@ variable "create" {
 variable "vpc_name" {
   description = "Nombre de la VPC"
   type        = string
-  default     = "vpc" # Puedes definir un valor predeterminado o dejarlo vacío para ser obligatorio
+
 }
 
 variable "cidr_block_vpc" {
-  type    = string
-  default = "10.0.0.0/16"
+  type = string
 }
 
 variable "region" {
@@ -24,31 +23,16 @@ variable "region" {
 variable "cidr_public_subnets" {
   description = "CIDR para las subredes públicas"
   type        = list(string)
-  default = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24"
-  ]
 }
 
 variable "cidr_private_subnets" {
   description = "CIDR para las subredes privadas"
   type        = list(string)
-  default = [
-    "10.0.4.0/24",
-    "10.0.5.0/24",
-    "10.0.6.0/24"
-  ]
 }
 
 variable "availability_zones" {
   description = "Lista de zonas de disponibilidad"
   type        = list(string)
-  default = [
-    "us-east-1a",
-    "us-east-1b",
-    "us-east-1c"
-  ]
 }
 
 
