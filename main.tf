e1e05f988b8628003e61f5c98c0aa650e0e69f55
eleuiese2 <luisclape@gmail.com>
2025-04-06 21:35:06 -0500

    Create module

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "main" {
-  count      = var.create ? 1 : 0
-  cidr_block = var.cidr_block_vpc
-  enable_dns_support = true
+  count                = var.create ? 1 : 0
+  cidr_block           = var.cidr_block_vpc
+  enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
     Name = "${var.vpc_name}-vpc"
@@ -10,10 +10,10 @@ resource "aws_vpc" "main" {
 
 # Crear subredes públicas
 resource "aws_subnet" "public" {
-  for_each          = var.create ? toset(var.cidr_public_subnets) : {}
-  vpc_id            = aws_vpc.main[0].id
-  cidr_block        = each.value
-  availability_zone = element(var.availability_zones, index(var.cidr_public_subnets, each.key))  # Usamos las zonas de disponibilidad de la variable
+  for_each                = var.create ? toset(var.cidr_public_subnets) : {}
+  vpc_id                  = aws_vpc.main[0].id
+  cidr_block              = each.value
+  availability_zone       = element(var.availability_zones, index(var.cidr_public_subnets, each.key)) # Usamos las zonas de disponibilidad de la variable
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet-${each.key}"
@@ -25,7 +25,7 @@ resource "aws_subnet" "private" {
   for_each          = var.create ? toset(var.cidr_private_subnets) : {}
   vpc_id            = aws_vpc.main[0].id
   cidr_block        = each.value
-  availability_zone = element(var.availability_zones, index(var.cidr_private_subnets, each.key))  # Usamos las zonas de disponibilidad de la variable
+  availability_zone = element(var.availability_zones, index(var.cidr_private_subnets, each.key)) # Usamos las zonas de disponibilidad de la variable
   tags = {
     Name = "private-subnet-${each.key}"
   }
@@ -33,7 +33,7 @@ resource "aws_subnet" "private" {
 
 # Crear Internet Gateway (para subredes públicas)
 resource "aws_internet_gateway" "internet_gateway" {
-  count = var.create ? 1 : 0
+  count  = var.create ? 1 : 0
   vpc_id = aws_vpc.main[0].id
   tags = {
     Name = "${var.vpc_name}-internet-gateway"
@@ -43,28 +43,28 @@ resource "aws_internet_gateway" "internet_gateway" {
 # Crear NAT Gateway para subredes privadas
 resource "aws_eip" "nat_gateway_eip" {
   count = var.create ? 1 : 0
-  vpc = true
+  vpc   = true
 }
 
 resource "aws_nat_gateway" "nat_gateway" {
-  count = var.create ? 1 : 0
+  count         = var.create ? 1 : 0
   allocation_id = aws_eip.nat_gateway_eip[0].id
   subnet_id     = aws_subnet.public[0].id
-  depends_on = [aws_eip.nat_gateway_eip]
+  depends_on    = [aws_eip.nat_gateway_eip]
 }
 
 # Tabla de enrutamiento para subredes públicas
 resource "aws_route_table" "public" {
-  count = var.create ? 1 : 0
+  count  = var.create ? 1 : 0
   vpc_id = aws_vpc.main[0].id
 }
 
 resource "aws_route" "internet_route" {
-  count = var.create ? 3 : 0
+  count                  = var.create ? 3 : 0
   route_table_id         = aws_route_table.public[0].id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.internet_gateway[0].id
-  depends_on = [aws_internet_gateway.internet_gateway]
+  depends_on             = [aws_internet_gateway.internet_gateway]
 }
 
 # Asociar subredes públicas con la tabla de enrutamiento pública
@@ -76,16 +76,16 @@ resource "aws_route_table_association" "public_association" {
 
 # Tabla de enrutamiento para subredes privadas
 resource "aws_route_table" "private" {
-  count = var.create ? 1 : 0
+  count  = var.create ? 1 : 0
   vpc_id = aws_vpc.main[0].id
 }
 
 resource "aws_route" "private_nat_route" {
-  count = var.create ? 3 : 0
+  count                  = var.create ? 3 : 0
   route_table_id         = aws_route_table.private[0].id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.nat_gateway[0].id
-  depends_on = [aws_nat_gateway.nat_gateway]
+  depends_on             = [aws_nat_gateway.nat_gateway]
 }
 
 # Asociar subredes privadas con la tabla de enrutamiento privada
--- variables.tf
+++ variables.tf
@@ -7,24 +7,24 @@ variable "create" {
 variable "vpc_name" {
   description = "Nombre de la VPC"
   type        = string
-  default     = "vpc"  # Puedes definir un valor predeterminado o dejarlo vacío para ser obligatorio
+  default     = "vpc" # Puedes definir un valor predeterminado o dejarlo vacío para ser obligatorio
 }
 
 variable "cidr_block_vpc" {
-  type = string
+  type    = string
   default = "10.0.0.0/16"
 }
 
 variable "region" {
   description = "La región de AWS donde se desplegarán los recursos"
   type        = string
-  default     = "us-east-1"  # Puedes definir un valor predeterminado o dejarlo vacío para ser obligatorio
+  default     = "us-east-1" # Puedes definir un valor predeterminado o dejarlo vacío para ser obligatorio
 }
 
 variable "cidr_public_subnets" {
   description = "CIDR para las subredes públicas"
   type        = list(string)
-  default     = [
+  default = [
     "10.0.1.0/24",
     "10.0.2.0/24",
     "10.0.3.0/24"
@@ -34,7 +34,7 @@ variable "cidr_public_subnets" {
 variable "cidr_private_subnets" {
   description = "CIDR para las subredes privadas"
   type        = list(string)
-  default     = [
+  default = [
     "10.0.4.0/24",
     "10.0.5.0/24",
     "10.0.6.0/24"
@@ -44,9 +44,9 @@ variable "cidr_private_subnets" {
 variable "availability_zones" {
   description = "Lista de zonas de disponibilidad"
   type        = list(string)
-  default     = [
-    "us-east-1a", 
-    "us-east-1b", 
+  default = [
+    "us-east-1a",
+    "us-east-1b",
     "us-east-1c"
   ]
 }
